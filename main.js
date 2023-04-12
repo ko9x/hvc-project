385638528975861fa45c55ee08ee012d956c3e4c
Sean McGillis <slcidevice@yahoo.com>
2023-04-12 16:48:16 -0600

    Added clearSearch button and cleaned up the code a bit

--- main.js
+++ main.js
@@ -1,17 +1,13 @@
 const resultSection = document.getElementById('resultSection');
 const searchItem = document.getElementById('searchItem');
 const executeSearch = document.getElementById('executeSearch');
+const clearSearch = document.getElementById('clearSearch');
 const invalidSerial = document.getElementById('invalidSerial');
 const systemDetails = document.getElementById('systemDetails');
 const partNumber = document.getElementById('partNumber');
 const errorMessage = '<h6>Please enter a valid Elite System Serial Number<h6>';
 
-function clearResults() {
-    systemDetails.innerText = '';
-    partNumber.innerText = '';
-    invalidSerial.innerText = '';
-};
-
+// Event Listeners
 searchItem.addEventListener('focus', () => {
     clearResults();
 });
@@ -24,9 +20,29 @@ searchItem.addEventListener('keypress', (e) => {
     }
 });
 
-function standardCLogic(standardSerial) {
-    let sequenceNum = standardSerial.slice(-5);
-    let configChars = standardSerial.slice(0, 6);
+executeSearch.addEventListener('click', () => {
+    findConfiguration();
+});
+
+clearSearch.addEventListener('click', () => {
+    searchItem.value = '';
+    clearResults();
+});
+
+function presentErrorMessage(uniqueMessage) {
+    invalidSerial.innerHTML = `<h5>Entered Serial Number ${uniqueMessage}</h5>${errorMessage}`;
+    systemDetails.innerHTML = '';
+    partNumber.innerHTML = '';
+}
+
+function clearResults() {
+    systemDetails.innerText = '';
+    partNumber.innerText = '';
+    invalidSerial.innerText = '';
+};
+
+// Standard C Configuration Logic
+function standardCLogic(standardSerial, configChars, sequenceNum) {
     if(standardSerial.charAt(4) !== 'T') {
         if(configChars === 'F9XXXX') {
             if (sequenceNum < '00035') {
@@ -75,16 +91,10 @@ function standardCLogic(standardSerial) {
             }
         }
     }
-    console.log('invalid serial number error', ); //@DEBUG
+    presentErrorMessage('is not valid');
 }
 
-function presentErrorMessage(uniqueMessage) {
-    invalidSerial.innerHTML = `<h5>Entered Serial Number ${uniqueMessage}</h5>${errorMessage}`;
-    systemDetails.innerHTML = '';
-    partNumber.innerHTML = '';
-}
-
-// Configuration Identification Logic
+// General Configuration Identification Logic
 function findConfiguration() {
     if(searchItem.value.length !== 11) {
         presentErrorMessage('is not 11 characters');
@@ -93,17 +103,10 @@ function findConfiguration() {
     if(capitalizedSearchItem.charAt(0) !== 'F') {
         presentErrorMessage("does not start with character 'F'");
     };
+    let sequenceNum = capitalizedSearchItem.slice(-5);
+    let configChars = capitalizedSearchItem.slice(0, 6);
     if(capitalizedSearchItem.charAt(1) === '9' || '2') {
-        standardCLogic(capitalizedSearchItem);
+        standardCLogic(capitalizedSearchItem, configChars, sequenceNum);
     }
 };
 
-executeSearch.addEventListener('click', () => {
-    findConfiguration();
-    // const systemDetailResult = document.createElement('h1');
-    // const partNumberResult = document.createElement('h1');
-    // systemDetailResult.innerHTML = 'Super C 9" Tablet';
-    // partNumberResult.innerHTML = '5214487';
-    // systemDetails.appendChild(systemDetailResult);
-    // partNumber.appendChild(partNumberResult);
-});
