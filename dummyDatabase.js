4b39e2caa4460ea5498e619b100b5e5aea95b4ad
Sean McGillis <slcidevice@yahoo.com>
2023-05-03 16:22:30 -0600

    Added breakPoint information for all the different configurations. Also fixed the configs to exclude the section where the T will go. We will handle tablet identification later

--- dummyDatabase.js
+++ dummyDatabase.js
@@ -31,13 +31,13 @@ const cutInfo = [
 export const configs = [
   { id: 1, code: "F9XX", description: 'Standard C 9" Image Intensifier' },
   { id: 2, code: "F2XX", description: 'Standard C 12" Image Intensifier' },
-  { id: 3, code: "FAXX", description: 'Ergo C 21cm Flat Panel Display' },
+  { id: 3, code: "FAXX", description: "Ergo C 21cm Flat Panel Display" },
   { id: 4, code: "FBXX", description: 'Ergo C 31cm" Flat Panel Display' },
   { id: 5, code: "FSXX", description: 'Super C 9" Image Intensifier' },
-  { id: 6, code: "FAHX", description: 'Super C 21cm Flat Panel Display' },
-  { id: 7, code: "FBHX", description: 'Super C 31cm Flat Panel Display' },
-  { id: 8, code: "FAMH", description: 'Motorized C 21cm Flat Panel Display' },
-  { id: 9, code: "FBMH", description: 'Motorized C 31cm Flat Panel Display' },
+  { id: 6, code: "FAHX", description: "Super C 21cm Flat Panel Display" },
+  { id: 7, code: "FBHX", description: "Super C 31cm Flat Panel Display" },
+  { id: 8, code: "FAMH", description: "Motorized C 21cm Flat Panel Display" },
+  { id: 9, code: "FBMH", description: "Motorized C 31cm Flat Panel Display" },
 ];
 
 // Here is an example of an itemInfo object that has the information filled out for configs.id 1 which is Standard 9"
@@ -59,97 +59,61 @@ export const itemInfo = {
     {
       id: 2,
       breakPoints: [
-        { startsAt: "F9XXXX00001", endsAt: "F9XXXX00034", display: "5443126" },
+        { startsAt: "F2XXXX00001", endsAt: "F2XXXX00001", display: "5443126" },
         {
-          startsAt: "F9XXXX00035",
-          endsAt: "F9XXTX00133",
+          startsAt: "F2XXXX00032",
+          endsAt: "F2XXTX00298",
           display: "5792202 (if unavailable, order 5877920)",
         },
-        { startsAt: "F9XXXX00035", endsAt: null, display: "5877920" },
+        { startsAt: "F2XXXX00032", endsAt: null, display: "5877920" },
       ],
     },
     {
       id: 3,
       breakPoints: [
-        { startsAt: "F9XXXX00001", endsAt: "F9XXXX00034", display: "5443126" },
-        {
-          startsAt: "F9XXXX00035",
-          endsAt: "F9XXTX00133",
-          display: "5792202 (if unavailable, order 5877920)",
-        },
-        { startsAt: "F9XXXX00035", endsAt: null, display: "5877920" },
+        { startsAt: "FAXXXE00001", endsAt: "FAXXTE00000", display: "5759488 (if unavailable, order 5792201)" },
+        { startsAt: "FAXXTE00001", endsAt: null, display: "5792201" },
       ],
     },
     {
       id: 4,
       breakPoints: [
-        { startsAt: "F9XXXX00001", endsAt: "F9XXXX00034", display: "5443126" },
-        {
-          startsAt: "F9XXXX00035",
-          endsAt: "F9XXTX00133",
-          display: "5792202 (if unavailable, order 5877920)",
-        },
-        { startsAt: "F9XXXX00035", endsAt: null, display: "5877920" },
+        { startsAt: "FBXXXE00001", endsAt: "FBXXTE00000", display: "5759488 (if unavailable, order 5792201)" },
+        { startsAt: "FBXXTE00001", endsAt: null, display: "5792201" },
       ],
     },
     {
       id: 5,
       breakPoints: [
-        { startsAt: "F9XXXX00001", endsAt: "F9XXXX00034", display: "5443126" },
-        {
-          startsAt: "F9XXXX00035",
-          endsAt: "F9XXTX00133",
-          display: "5792202 (if unavailable, order 5877920)",
-        },
-        { startsAt: "F9XXXX00035", endsAt: null, display: "5877920" },
+        { startsAt: "FSXXXX00001", endsAt: null, display: "5443126" },
       ],
     },
     {
       id: 6,
       breakPoints: [
-        { startsAt: "F9XXXX00001", endsAt: "F9XXXX00034", display: "5443126" },
-        {
-          startsAt: "F9XXXX00035",
-          endsAt: "F9XXTX00133",
-          display: "5792202 (if unavailable, order 5877920)",
-        },
-        { startsAt: "F9XXXX00035", endsAt: null, display: "5877920" },
+        { startsAt: "FAHXXX00001", endsAt: "FAHXXX00106", display: "5458880" },
+        { startsAt: "FAHXXX00107", endsAt: null, display: "5790094" },
       ],
     },
     {
       id: 7,
       breakPoints: [
-        { startsAt: "F9XXXX00001", endsAt: "F9XXXX00034", display: "5443126" },
-        {
-          startsAt: "F9XXXX00035",
-          endsAt: "F9XXTX00133",
-          display: "5792202 (if unavailable, order 5877920)",
-        },
-        { startsAt: "F9XXXX00035", endsAt: null, display: "5877920" },
+        { startsAt: "FBHXXX00001", endsAt: "FBHXXX00195", display: "5458880" },
+        { startsAt: "FBHXXX00196", endsAt: null, display: "5790094" },
       ],
     },
     {
       id: 8,
       breakPoints: [
-        { startsAt: "F9XXXX00001", endsAt: "F9XXXX00034", display: "5443126" },
-        {
-          startsAt: "F9XXXX00035",
-          endsAt: "F9XXTX00133",
-          display: "5792202 (if unavailable, order 5877920)",
-        },
-        { startsAt: "F9XXXX00035", endsAt: null, display: "5877920" },
+        { startsAt: "FAMHXX00001", endsAt: "FAMHXX00009", display: "5428385" },
+        { startsAt: "FAMHXX00010", endsAt: null, display: "5786472" },
       ],
     },
     {
       id: 9,
       breakPoints: [
-        { startsAt: "F9XXXX00001", endsAt: "F9XXXX00034", display: "5443126" },
-        {
-          startsAt: "F9XXXX00035",
-          endsAt: "F9XXTX00133",
-          display: "5792202 (if unavailable, order 5877920)",
-        },
-        { startsAt: "F9XXXX00035", endsAt: null, display: "5877920" },
+        { startsAt: "FBMHXX00001", endsAt: "FBMHXX00020", display: "5428385" },
+        { startsAt: "FBMHXX00021", endsAt: null, display: "5786472" },
       ],
     },
   ],
