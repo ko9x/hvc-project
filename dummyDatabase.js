76ffb881ae5682f9693acf9579ffc1d7ee88d3c8
Sean McGillis <slcidevice@yahoo.com>
2023-05-08 15:23:46 -0600

    Formatting and cleanup

--- dummyDatabase.js
+++ dummyDatabase.js
@@ -1,38 +1,8 @@
-// I don't think we will go this route but leaving these two here for now
-// const data = {items: [{id: 1, name: 'High Voltage Cable', CutInfo: [{}]}, {name: 'ebox'}]};
-// const items = [{id: 1, name: 'High Voltage Cable', breakPoints: [{config1: []}], partInformation: [] }];
-
-// The cutInfo object will hold the cutIn information for each configuration of Elite C Arm. ConfigID: 1 is standard 9"
-//  Each breakPoint will have an id, startsAt, endsAt, and display. There must be at least one breakPoint for each config.
-const cutInfo = [
-  {
-    configID: 1,
-    breakPoints: [
-      {
-        id: 1,
-        startsAt: "F9XXXX00001",
-        endsAt: "F9XXXX00034",
-        display: "5443126",
-      },
-      {
-        id: 2,
-        startsAt: "F9XXXX00035",
-        endsAt: "F9XXTX00133",
-        display: "5792202 (if unavailable, order 5877920)",
-      },
-      { id: 3, startsAt: "F9XXXX00035", endsAt: null, display: "5877920" },
-    ],
-  },
-];
-
-// *********************** Info above this line is just brainstorming *******************************************
-
-// The configs object will just show the description of the C based on the 6 digit serial "code"
 export const configs = [
   { id: 1, code: "F9XX", description: 'Standard C 9" Image Intensifier' },
   { id: 2, code: "F2XX", description: 'Standard C 12" Image Intensifier' },
   { id: 3, code: "FAXX", description: "Ergo C 21cm Flat Panel Display" },
-  { id: 4, code: "FBXX", description: 'Ergo C 31cm" Flat Panel Display' },
+  { id: 4, code: "FBXX", description: "Ergo C 31cm Flat Panel Display" },
   { id: 5, code: "FSXX", description: 'Super C 9" Image Intensifier' },
   { id: 6, code: "FAHX", description: "Super C 21cm Flat Panel Display" },
   { id: 7, code: "FBHX", description: "Super C 31cm Flat Panel Display" },
@@ -40,7 +10,6 @@ export const configs = [
   { id: 9, code: "FBMH", description: "Motorized C 31cm Flat Panel Display" },
 ];
 
-// Here is an example of an items object that has the information filled out for configs.id 1 which is Standard 9"
 export const items = [
   {
     name: "High Voltage Cable Part#:",
@@ -58,7 +27,11 @@ export const items = [
             endsAt: "F9XXTX00133",
             display: "5792202 (if unavailable, order 5877920)",
           },
-          { startsAt: "F9XXXX00035", endsAt: 'F9XXTX99999', display: "5877920" },
+          {
+            startsAt: "F9XXXX00035",
+            endsAt: "F9XXTX99999",
+            display: "5877920",
+          },
         ],
       },
       {
@@ -74,7 +47,11 @@ export const items = [
             endsAt: "F2XXTX00298",
             display: "5792202 (if unavailable, order 5877920)",
           },
-          { startsAt: "F2XXXX00032", endsAt: "F2XXTX99999", display: "5877920" },
+          {
+            startsAt: "F2XXXX00032",
+            endsAt: "F2XXTX99999",
+            display: "5877920",
+          },
         ],
       },
       {
@@ -85,7 +62,11 @@ export const items = [
             endsAt: "FAXXXE99999",
             display: "5759488 (if unavailable, order 5792201)",
           },
-          { startsAt: "FAXXTE00001", endsAt: "FAXXTE99999", display: "5792201" },
+          {
+            startsAt: "FAXXTE00001",
+            endsAt: "FAXXTE99999",
+            display: "5792201",
+          },
         ],
       },
       {
@@ -96,13 +77,21 @@ export const items = [
             endsAt: "FBXXXE99999",
             display: "5759488 (if unavailable, order 5792201)",
           },
-          { startsAt: "FBXXTE00001", endsAt: "FBXXTE99999", display: "5792201" },
+          {
+            startsAt: "FBXXTE00001",
+            endsAt: "FBXXTE99999",
+            display: "5792201",
+          },
         ],
       },
       {
         id: 5,
         breakPoints: [
-          { startsAt: "FSXXXX00001", endsAt: "FSXXTX99999", display: "5443126" },
+          {
+            startsAt: "FSXXXX00001",
+            endsAt: "FSXXTX99999",
+            display: "5443126",
+          },
         ],
       },
       {
@@ -113,7 +102,11 @@ export const items = [
             endsAt: "FAHXXX00106",
             display: "5458880",
           },
-          { startsAt: "FAHXXX00107", endsAt: "FAHXTX99999", display: "5790094" },
+          {
+            startsAt: "FAHXXX00107",
+            endsAt: "FAHXTX99999",
+            display: "5790094",
+          },
         ],
       },
       {
@@ -124,7 +117,11 @@ export const items = [
             endsAt: "FBHXXX00195",
             display: "5458880",
           },
-          { startsAt: "FBHXXX00196", endsAt: "FBHXTX99999", display: "5790094" },
+          {
+            startsAt: "FBHXXX00196",
+            endsAt: "FBHXTX99999",
+            display: "5790094",
+          },
         ],
       },
       {
@@ -135,7 +132,11 @@ export const items = [
             endsAt: "FAMHXX00009",
             display: "5428385",
           },
-          { startsAt: "FAMHXX00010", endsAt: "FAMHTX99999", display: "5786472" },
+          {
+            startsAt: "FAMHXX00010",
+            endsAt: "FAMHTX99999",
+            display: "5786472",
+          },
         ],
       },
       {
@@ -146,7 +147,11 @@ export const items = [
             endsAt: "FBMHXX00020",
             display: "5428385",
           },
-          { startsAt: "FBMHXX00021", endsAt: "FBMHTX99999", display: "5786472" },
+          {
+            startsAt: "FBMHXX00021",
+            endsAt: "FBMHTX99999",
+            display: "5786472",
+          },
         ],
       },
     ],
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import {items, configs} from './dummyDatabase.js'
+import { items, configs } from "./dummyDatabase.js";
 
 const container = document.getElementById("container");
 const titleText = document.getElementById("titleText");
@@ -7,7 +7,9 @@ const subTitleText = document.getElementById("subTitleText");
 const searchSection = document.getElementById("searchSection");
 const searchItem = document.getElementById("searchItem");
 const executeSearch = document.getElementById("executeSearch");
-const resultSectionContainer = document.getElementById("resultSectionContainer");
+const resultSectionContainer = document.getElementById(
+  "resultSectionContainer"
+);
 const resultSection = document.getElementById("resultSection");
 const topResultLabel = document.getElementById("topResultLabel");
 const topResultContent = document.getElementById("topResultContent");
@@ -15,12 +17,14 @@ const bottomResultLabel = document.getElementById("bottomResultLabel");
 const bottomResultContent = document.getElementById("bottomResultContent");
 
 // Constants for height and width
-const width = window.innerWidth
-|| document.documentElement.clientWidth
-|| document.body.clientWidth;
-const height = window.innerHeight
-|| document.documentElement.clientHeight
-|| document.body.clientHeight;
+const width =
+  window.innerWidth ||
+  document.documentElement.clientWidth ||
+  document.body.clientWidth;
+const height =
+  window.innerHeight ||
+  document.documentElement.clientHeight ||
+  document.body.clientHeight;
 
 // Create a variable to differentiate between large and small screens
 const isSmall = width < 750;
@@ -30,7 +34,7 @@ hideResultsSection();
 handleLayout();
 
 // Add and remove the resultSectionContainer class to hide a weird flash when the page loads
-resultSectionContainer.classList.remove('resultSectionContainer');
+resultSectionContainer.classList.remove("resultSectionContainer");
 
 // Event Listeners
 searchItem.addEventListener("focus", () => {
@@ -52,33 +56,33 @@ executeSearch.addEventListener("click", () => {
 
 // Handle layout based on the screen size
 function handleLayout() {
-  const titleTextDescription = 'Elite High Voltage Cable Finder'
-  const subTitleTextDescription = 'Identify the correct High Voltage Cable part number using the System Serial Number'
-  if(!isSmall) {
-    container.classList.add('largeContainer');
-    titleSection.classList.add('largeTitleSection');
-    searchSection.classList.add('largeSearchSection');
-    resultSection.classList.add('largeResultSection');
+  const titleTextDescription = "Elite High Voltage Cable Finder";
+  const subTitleTextDescription =
+    "Identify the correct High Voltage Cable part number using the System Serial Number";
+  if (!isSmall) {
+    container.classList.add("largeContainer");
+    titleSection.classList.add("largeTitleSection");
+    searchSection.classList.add("largeSearchSection");
+    resultSection.classList.add("largeResultSection");
     titleText.innerHTML = `<h1>${titleTextDescription}</h1>`;
-    subTitleText.innerHTML = `<h3>${subTitleTextDescription}</h3>`
-
+    subTitleText.innerHTML = `<h3>${subTitleTextDescription}</h3>`;
   }
-  if(isSmall) {
-    container.classList.add('smallContainer');
-    executeSearch.classList.add('smallButton');
+  if (isSmall) {
+    container.classList.add("smallContainer");
+    executeSearch.classList.add("smallButton");
     titleText.innerHTML = `<h3>${titleTextDescription}</h3>`;
-    subTitleText.innerHTML = `<p>${subTitleTextDescription}</p>`
+    subTitleText.innerHTML = `<p>${subTitleTextDescription}</p>`;
   }
 }
 
 // Make the search button change color for 100ms when the user clicks it
 function flashSearchButton() {
-  executeSearch.style.backgroundColor = 'rgb(100, 0, 160)';
-    executeSearch.style.color = 'white';
-    setTimeout(() => {
-        executeSearch.style.color = 'black';
-        executeSearch.style.backgroundColor = 'white'
-    }, 100)
+  executeSearch.style.backgroundColor = "rgb(100, 0, 160)";
+  executeSearch.style.color = "white";
+  setTimeout(() => {
+    executeSearch.style.color = "black";
+    executeSearch.style.backgroundColor = "white";
+  }, 100);
 }
 
 function clearResults() {
@@ -90,19 +94,19 @@ function clearResults() {
 }
 
 function showResultsSection() {
-  resultSection.classList.remove('resultSectionHide');
+  resultSection.classList.remove("resultSectionHide");
 }
 
 function hideResultsSection() {
-    resultSection.classList.add('resultSectionHide');
+  resultSection.classList.add("resultSectionHide");
 }
 
 function displayResults(id, name, itemInfo, userInterface) {
-  const configuration = configs.find(config => config.id === id)
-    topResultLabel.innerText = "System Configuration Details:";
-    bottomResultLabel.innerText = name;
-    topResultContent.innerText = `${configuration.description} ${userInterface}` ;
-    bottomResultContent.innerText = itemInfo;
+  const configuration = configs.find((config) => config.id === id);
+  topResultLabel.innerText = "System Configuration Details:";
+  bottomResultLabel.innerText = name;
+  topResultContent.innerText = `${configuration.description} ${userInterface}`;
+  bottomResultContent.innerText = itemInfo;
   showResultsSection();
   return;
 }
@@ -110,7 +114,7 @@ function displayResults(id, name, itemInfo, userInterface) {
 function validateSerialNumber() {
   let regex = /F[ABS29][HMX][HX][TX][EX]\d\d\d\d\d/i;
   let capitalizedSearchItem = searchItem.value.toUpperCase();
-  if(capitalizedSearchItem.match(regex)) {
+  if (capitalizedSearchItem.match(regex)) {
     findConfiguration(capitalizedSearchItem);
   } else {
     displayResults("The Serial Number entered is not valid");
@@ -118,34 +122,49 @@ function validateSerialNumber() {
 }
 
 function findBreakPoint(serialNum, breakPointArr, sequenceNum, id, name) {
-  if(serialNum.charAt(4) === 'T') {
+  if (serialNum.charAt(4) === "T") {
     const infoBreakPoint = breakPointArr.find((breakPoint) => {
       let startSequenceNum = breakPoint.startsAt.slice(-5);
       let endSequenceNum = breakPoint.endsAt.slice(-5);
-      if(breakPoint.startsAt.charAt(4) === 'X') {
-        if(breakPoint.endsAt.charAt(4) === 'T' && endSequenceNum >= sequenceNum) {
+      if (breakPoint.startsAt.charAt(4) === "X") {
+        if (
+          breakPoint.endsAt.charAt(4) === "T" &&
+          endSequenceNum >= sequenceNum
+        ) {
           return breakPoint;
         }
       }
-      if(breakPoint.startsAt.charAt(4) === 'T') {
-        if(startSequenceNum <= sequenceNum && endSequenceNum >= sequenceNum) {
+      if (breakPoint.startsAt.charAt(4) === "T") {
+        if (startSequenceNum <= sequenceNum && endSequenceNum >= sequenceNum) {
           return breakPoint;
         }
       }
-    })
-    displayResults(id, name, infoBreakPoint.display, 'with Tablet');
+    });
+    displayResults(id, name, infoBreakPoint.display, "with Tablet");
   }
-  if(serialNum.charAt(4) === 'X') {
+  if (serialNum.charAt(4) === "X") {
     const infoBreakPoint = breakPointArr.find((breakPoint) => {
       let startSequenceNum = breakPoint.startsAt.slice(-5);
       let endSequenceNum = breakPoint.endsAt.slice(-5);
-      if(breakPoint.startsAt.charAt(4) === 'X' && startSequenceNum <= sequenceNum) {
-        if ((breakPoint.endsAt.charAt(4) === 'X' && endSequenceNum >= sequenceNum) || breakPoint.endsAt.charAt(4) === 'T') {
+      if (
+        breakPoint.startsAt.charAt(4) === "X" &&
+        startSequenceNum <= sequenceNum
+      ) {
+        if (
+          (breakPoint.endsAt.charAt(4) === "X" &&
+            endSequenceNum >= sequenceNum) ||
+          breakPoint.endsAt.charAt(4) === "T"
+        ) {
           return breakPoint;
         }
       }
-    })
-    displayResults(id, name, infoBreakPoint.display, 'with Control Panel (non-tablet)');
+    });
+    displayResults(
+      id,
+      name,
+      infoBreakPoint.display,
+      "with Control Panel (non-tablet)"
+    );
   }
 }
 
@@ -154,14 +173,20 @@ function findConfiguration(capitalizedSearchItem) {
   let configChars = capitalizedSearchItem.slice(0, 4);
   let sequenceNum = capitalizedSearchItem.slice(-5);
   configs.find((config) => {
-    if(config.code === configChars) {
+    if (config.code === configChars) {
       items.forEach((item) => {
         item.configs.find((itemConfig) => {
-          if(config.id === itemConfig.id) {
-            findBreakPoint(capitalizedSearchItem, itemConfig.breakPoints, sequenceNum, config.id, item.name);
+          if (config.id === itemConfig.id) {
+            findBreakPoint(
+              capitalizedSearchItem,
+              itemConfig.breakPoints,
+              sequenceNum,
+              config.id,
+              item.name
+            );
           }
-        })
-      })
+        });
+      });
     }
-  })
+  });
 }
