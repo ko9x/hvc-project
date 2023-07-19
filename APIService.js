116012e2c55c47459c9622685ec8bf17f5eca131
Sean McGillis <slcidevice@yahoo.com>
2023-07-19 16:52:10 -0600

    Clear the form when an item is successfully added to the database

--- APIService.js
+++ APIService.js
@@ -1,8 +1,8 @@
 // API using localhost
-// const API = 'http://localhost:8080/api/item'
+const API = 'http://localhost:8080/api/item'
 
 // API using MacBook IP Address (So I can test on my iphone)
-const API = 'http://192.168.0.17:8080/api/item'
+// const API = 'http://192.168.0.17:8080/api/item'
 
 export async function getItems(config) {
     let response = await fetch(`${API}/${config}`);
@@ -27,8 +27,11 @@ export async function storeItem(data) {
     const content = await response.json();
 
     if(content?.success) {
-        alert(`Item: ${data.name} has been stored successfully`);
+        // Clear the form (by reloading the screen) when the user clicks the "ok" button in the success = true alert
+        if(alert(`Item: ${data.name} has been stored successfully`)){}
+        else window.location.reload();
     } else {
+        // Don't clear the form when the user clicks the "ok" button in the success = false alert
         alert(`Something went wrong`);
     }
 }
